96dab3c23aa41f9d0fad0405859073c7caaad58c
Joan Maria Talarn Espelta <jmtalarn@gmail.com>
2021-02-09 21:49:13 +0100

    Path routing on github pages

--- src/Spa/Generated/Route.elm
+++ src/Spa/Generated/Route.elm
@@ -50,43 +50,48 @@ fromUrl =
     Parser.parse routes
 
 
+basepath : String
+basepath =
+    "30-days-of-elm"
+
+
 routes : Parser (Route -> a) a
 routes =
     Parser.oneOf
-        [ Parser.map Top Parser.top
-        , Parser.map Day1 (Parser.s "day1")
-        , Parser.map Day10 (Parser.s "day10")
-        , Parser.map Day11 (Parser.s "day11")
-        , Parser.map Day12 (Parser.s "day12")
-        , Parser.map Day13 (Parser.s "day13")
-        , Parser.map Day14 (Parser.s "day14")
-        , Parser.map Day15 (Parser.s "day15")
-        , Parser.map Day16 (Parser.s "day16")
-        , Parser.map Day17 (Parser.s "day17")
-        , Parser.map Day18 (Parser.s "day18")
-        , Parser.map Day19 (Parser.s "day19")
-        , Parser.map Day2 (Parser.s "day2")
-        , Parser.map Day20 (Parser.s "day20")
-        , Parser.map Day21 (Parser.s "day21")
-        , Parser.map Day22 (Parser.s "day22")
-        , Parser.map Day23 (Parser.s "day23")
-        , Parser.map Day24 (Parser.s "day24")
-        , Parser.map Day25 (Parser.s "day25")
-        , Parser.map Day26 (Parser.s "day26")
-        , Parser.map Day27 (Parser.s "day27")
-        , Parser.map Day28 (Parser.s "day28")
-        , Parser.map Day29 (Parser.s "day29")
-        , Parser.map Day3 (Parser.s "day3")
-        , Parser.map Day30 (Parser.s "day30")
-        , Parser.map Day4 (Parser.s "day4")
-        , Parser.map Day5 (Parser.s "day5")
-        , Parser.map Day6 (Parser.s "day6")
-        , Parser.map Day7 (Parser.s "day7")
-        , Parser.map Day8 (Parser.s "day8")
-        , Parser.map Day9 (Parser.s "day9")
-        , Parser.map NotFound (Parser.s "not-found")
-        , Parser.map PageA (Parser.s "page-a")
-        , Parser.map PageB (Parser.s "page-b")
+        [ Parser.map Top (Parser.s basepath) --Parser.top
+        , Parser.map Day1 (Parser.s basepath </> Parser.s "day1")
+        , Parser.map Day10 (Parser.s basepath </> Parser.s "day10")
+        , Parser.map Day11 (Parser.s basepath </> Parser.s "day11")
+        , Parser.map Day12 (Parser.s basepath </> Parser.s "day12")
+        , Parser.map Day13 (Parser.s basepath </> Parser.s "day13")
+        , Parser.map Day14 (Parser.s basepath </> Parser.s "day14")
+        , Parser.map Day15 (Parser.s basepath </> Parser.s "day15")
+        , Parser.map Day16 (Parser.s basepath </> Parser.s "day16")
+        , Parser.map Day17 (Parser.s basepath </> Parser.s "day17")
+        , Parser.map Day18 (Parser.s basepath </> Parser.s "day18")
+        , Parser.map Day19 (Parser.s basepath </> Parser.s "day19")
+        , Parser.map Day2 (Parser.s basepath </> Parser.s "day2")
+        , Parser.map Day20 (Parser.s basepath </> Parser.s "day20")
+        , Parser.map Day21 (Parser.s basepath </> Parser.s "day21")
+        , Parser.map Day22 (Parser.s basepath </> Parser.s "day22")
+        , Parser.map Day23 (Parser.s basepath </> Parser.s "day23")
+        , Parser.map Day24 (Parser.s basepath </> Parser.s "day24")
+        , Parser.map Day25 (Parser.s basepath </> Parser.s "day25")
+        , Parser.map Day26 (Parser.s basepath </> Parser.s "day26")
+        , Parser.map Day27 (Parser.s basepath </> Parser.s "day27")
+        , Parser.map Day28 (Parser.s basepath </> Parser.s "day28")
+        , Parser.map Day29 (Parser.s basepath </> Parser.s "day29")
+        , Parser.map Day3 (Parser.s basepath </> Parser.s "day3")
+        , Parser.map Day30 (Parser.s basepath </> Parser.s "day30")
+        , Parser.map Day4 (Parser.s basepath </> Parser.s "day4")
+        , Parser.map Day5 (Parser.s basepath </> Parser.s "day5")
+        , Parser.map Day6 (Parser.s basepath </> Parser.s "day6")
+        , Parser.map Day7 (Parser.s basepath </> Parser.s "day7")
+        , Parser.map Day8 (Parser.s basepath </> Parser.s "day8")
+        , Parser.map Day9 (Parser.s basepath </> Parser.s "day9")
+        , Parser.map NotFound (Parser.s basepath </> Parser.s "not-found")
+        , Parser.map PageA (Parser.s basepath </> Parser.s "page-a")
+        , Parser.map PageB (Parser.s basepath </> Parser.s "page-b")
         ]
 
 
@@ -97,106 +102,106 @@ toString route =
         segments =
             case route of
                 Top ->
-                    []
-                
+                    [ basepath ]
+
                 Day1 ->
-                    [ "day1" ]
-                
+                    [ basepath, "day1" ]
+
                 Day10 ->
-                    [ "day10" ]
-                
+                    [ basepath, "day10" ]
+
                 Day11 ->
-                    [ "day11" ]
-                
+                    [ basepath, "day11" ]
+
                 Day12 ->
-                    [ "day12" ]
-                
+                    [ basepath, "day12" ]
+
                 Day13 ->
-                    [ "day13" ]
-                
+                    [ basepath, "day13" ]
+
                 Day14 ->
-                    [ "day14" ]
-                
+                    [ basepath, "day14" ]
+
                 Day15 ->
-                    [ "day15" ]
-                
+                    [ basepath, "day15" ]
+
                 Day16 ->
-                    [ "day16" ]
-                
+                    [ basepath, "day16" ]
+
                 Day17 ->
-                    [ "day17" ]
-                
+                    [ basepath, "day17" ]
+
                 Day18 ->
-                    [ "day18" ]
-                
+                    [ basepath, "day18" ]
+
                 Day19 ->
-                    [ "day19" ]
-                
+                    [ basepath, "day19" ]
+
                 Day2 ->
-                    [ "day2" ]
-                
+                    [ basepath, "day2" ]
+
                 Day20 ->
-                    [ "day20" ]
-                
+                    [ basepath, "day20" ]
+
                 Day21 ->
-                    [ "day21" ]
-                
+                    [ basepath, "day21" ]
+
                 Day22 ->
-                    [ "day22" ]
-                
+                    [ basepath, "day22" ]
+
                 Day23 ->
-                    [ "day23" ]
-                
+                    [ basepath, "day23" ]
+
                 Day24 ->
-                    [ "day24" ]
-                
+                    [ basepath, "day24" ]
+
                 Day25 ->
-                    [ "day25" ]
-                
+                    [ basepath, "day25" ]
+
                 Day26 ->
-                    [ "day26" ]
-                
+                    [ basepath, "day26" ]
+
                 Day27 ->
-                    [ "day27" ]
-                
+                    [ basepath, "day27" ]
+
                 Day28 ->
-                    [ "day28" ]
-                
+                    [ basepath, "day28" ]
+
                 Day29 ->
-                    [ "day29" ]
-                
+                    [ basepath, "day29" ]
+
                 Day3 ->
-                    [ "day3" ]
-                
+                    [ basepath, "day3" ]
+
                 Day30 ->
-                    [ "day30" ]
-                
+                    [ basepath, "day30" ]
+
                 Day4 ->
-                    [ "day4" ]
-                
+                    [ basepath, "day4" ]
+
                 Day5 ->
-                    [ "day5" ]
-                
+                    [ basepath, "day5" ]
+
                 Day6 ->
-                    [ "day6" ]
-                
+                    [ basepath, "day6" ]
+
                 Day7 ->
-                    [ "day7" ]
-                
+                    [ basepath, "day7" ]
+
                 Day8 ->
-                    [ "day8" ]
-                
+                    [ basepath, "day8" ]
+
                 Day9 ->
-                    [ "day9" ]
-                
+                    [ basepath, "day9" ]
+
                 NotFound ->
-                    [ "not-found" ]
-                
+                    [ basepath, "not-found" ]
+
                 PageA ->
-                    [ "page-a" ]
-                
+                    [ basepath, "page-a" ]
+
                 PageB ->
-                    [ "page-b" ]
+                    [ basepath, "page-b" ]
     in
     segments
         |> String.join "/"
