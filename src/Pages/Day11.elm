98750e8ecbc628db2316e19142db4dfcda48ae26
Joan Maria Talarn Espelta <jmtalarn@gmail.com>
2021-02-11 16:00:16 +0100

    Day 11

--- src/Pages/Day11.elm
+++ src/Pages/Day11.elm
@@ -4,16 +4,18 @@ module Pages.Day11 exposing (..)
 
 import Colors.Opaque exposing (grey)
 import Element exposing (..)
-import Element.Background as Background
-import Element.Border as Border
 import Element.Font as Font exposing (size)
 import Element.Input as Input exposing (..)
-import Html exposing (h1, p)
+import Html exposing (h1, h3)
+import Html.Attributes as HtmlAttributes exposing (type_)
 import Html.Events exposing (onInput)
+import List exposing (filter, foldl)
+import ParseInt exposing (parseInt)
 import Shared
 import Spa.Document exposing (Document)
 import Spa.Page as Page exposing (Page)
 import Spa.Url exposing (Url)
+import String exposing (fromInt, split)
 
 
 page : Page Params Model Msg
@@ -33,28 +35,71 @@ type alias Params =
 
 
 type alias Model =
-    Float
+    { number : Int, numberBinay : Int, nextNumber : Int, nextNumberBinary : Int }
 
 
 init : Shared.Model -> Url Params -> ( Model, Cmd Msg )
 init shared { params } =
-    ( 0, Cmd.none )
+    ( Model 0 0 0 0, Cmd.none )
 
 
 type Msg
-    = Set Float
+    = Set String
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg _ =
     case msg of
         Set value ->
-            ( value, Cmd.none )
+            let
+                number =
+                    Result.withDefault 0 <| parseInt value
+
+                binaryNumber =
+                    number2binary <| number
+
+                nextNumber =
+                    nextNumberWithSameAmountOfOnes number (number + 1)
+
+                binaryNextNumber =
+                    number2binary <| nextNumber
+            in
+            ( Model number binaryNumber nextNumber binaryNextNumber, Cmd.none )
 
 
 save : Model -> Shared.Model -> Shared.Model
 save model shared =
     shared
+
+
+number2binary : Int -> Int
+number2binary number =
+    if number == 0 then
+        number
+
+    else
+        modBy 2 number + 10 * number2binary (number // 2)
+
+
+countOnes : Int -> Int
+countOnes number =
+    foldl (+) 0 <| filter (\a -> a == 1) <| List.map (Result.withDefault 0) <| List.map parseInt <| (fromInt number |> split "")
+
+
+nextNumberWithSameAmountOfOnes : Int -> Int -> Int
+nextNumberWithSameAmountOfOnes number nextNumber =
+    let
+        numberOnes =
+            countOnes <| number2binary number
+
+        nextNumberOnes =
+            countOnes <| number2binary nextNumber
+    in
+    if numberOnes == nextNumberOnes then
+        nextNumber
+
+    else
+        nextNumberWithSameAmountOfOnes number (nextNumber + 1)
 
 
 load : Shared.Model -> Model -> ( Model, Cmd Msg )
@@ -69,7 +114,7 @@ subscriptions model =
 
 view : Model -> Document Msg
 view model =
-    { title = "Day1"
+    { title = "Day 11"
     , body =
         [ column
             [ centerX
@@ -77,7 +122,33 @@ view model =
             , Font.size 30
             ]
             [ row [] [ html <| h1 [] [ Html.text "Day 11" ] ]
-            , row [] [ html <| p [] [ Html.text "Nothing here yet for this day of challenge" ] ]
+            , row [] [ html <| h3 [] [ Html.text "Next number with same amount of ones in binary representation" ] ]
+            , row [ spacing 20 ]
+                [ column
+                    [ spacing 10, centerY ]
+                    [ Input.text
+                        [ htmlAttribute <| type_ "number"
+                        , Font.extraLight
+                        ]
+                        { onChange = Set
+                        , text = String.fromInt model.number
+                        , placeholder = Just (Input.placeholder [] (Element.text "Write a number here"))
+                        , label =
+                            Input.labelAbove []
+                                (Element.text "Write a number here")
+                        }
+                    ]
+                , column [ spacing 15 ]
+                    [ Element.text "Your number in binary representation"
+                    , el [ Font.extraLight ]
+                        (Element.text <| String.fromInt model.numberBinay)
+                    , Element.text
+                        "Next number with same amount of ones"
+                    , el [ Font.extraLight ] (Element.text <| String.fromInt model.nextNumber)
+                    , Element.text "Number above in binary representation"
+                    , el [ Font.extraLight ] (Element.text <| String.fromInt model.nextNumberBinary)
+                    ]
+                ]
             ]
         ]
     }
