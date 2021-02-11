e4fd6c2aa2fe5feabbe1194db29885f4daadb482
Joan Maria Talarn Espelta <jmtalarn@gmail.com>
2021-02-11 22:24:34 +0100

    Day 13

--- src/Pages/Day13.elm
+++ src/Pages/Day13.elm
@@ -1,7 +1,9 @@
 module Pages.Day13 exposing (..)
 
 -- import Html.Attributes exposing (..)
+-- import Pages.Day12 exposing (nextNumberWithSameAmountOfOnes)
 
+import Binary
 import Colors.Opaque exposing (grey)
 import Element exposing (..)
 import Element.Background as Background
@@ -10,10 +12,27 @@ import Element.Font as Font exposing (size)
 import Element.Input as Input exposing (..)
 import Html exposing (h1, p)
 import Html.Events exposing (onInput)
+import LineChart exposing (..)
+import LineChart.Area as Area
+import LineChart.Axis as Axis
+import LineChart.Axis.Intersection as Intersection
+import LineChart.Colors as LCColors
+import LineChart.Container as Container
+import LineChart.Dots as Dots
+import LineChart.Events as Events
+import LineChart.Grid as Grid
+import LineChart.Interpolation as Interpolation
+import LineChart.Junk as Junk
+import LineChart.Legends as Legends
+import LineChart.Line as Line
+import List exposing (indexedMap)
+import Pages.Day12 exposing (nextNumberWithSameAmountOfOnes)
 import Shared
 import Spa.Document exposing (Document)
 import Spa.Page as Page exposing (Page)
 import Spa.Url exposing (Url)
+import Svg exposing (Svg)
+import Tuple exposing (first, second)
 
 
 page : Page Params Model Msg
@@ -33,23 +52,43 @@ type alias Params =
 
 
 type alias Model =
-    Float
+    ( List ( Float, Float ), List ( Float, Float ) )
+
+
+getNextNumber : Int -> Int
+getNextNumber number =
+    Binary.toDecimal <| nextNumberWithSameAmountOfOnes (Binary.fromDecimal number) (Binary.fromDecimal (number + 1))
 
 
 init : Shared.Model -> Url Params -> ( Model, Cmd Msg )
 init shared { params } =
-    ( 0, Cmd.none )
+    let
+        range =
+            List.range 1 100
+
+        nextRange =
+            List.map getNextNumber range
+
+        floatsRange =
+            List.map toFloat range
+
+        floatsNextRange =
+            List.map toFloat nextRange
+    in
+    ( ( List.map (\( a, b ) -> ( toFloat a, b )) <| indexedMap Tuple.pair floatsRange
+      , List.map (\( a, b ) -> ( toFloat a, b )) <| indexedMap Tuple.pair floatsNextRange
+      )
+    , Cmd.none
+    )
 
 
 type Msg
-    = Set Float
+    = NoOp
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
-update msg _ =
-    case msg of
-        Set value ->
-            ( value, Cmd.none )
+update _ model =
+    ( model, Cmd.none )
 
 
 save : Model -> Shared.Model -> Shared.Model
@@ -69,15 +108,38 @@ subscriptions model =
 
 view : Model -> Document Msg
 view model =
-    { title = "Day1"
+    { title = "Day 13"
     , body =
         [ column
             [ centerX
             , padding 40
             , Font.size 30
             ]
-            [ row [] [ html <| h1 [] [ Html.text "Day 13" ] ]
-            , row [] [ html <| p [] [ Html.text "Nothing here yet for this day of challenge" ] ]
+            [ row [] [ html <| h1 [] [ Html.text "Day 13 - Visualize pair of values of Day 12" ] ]
+
+            -- , row [] [ Element.paragraph [] (List.map (\( a, b ) -> Element.text <| "[" ++ String.fromFloat a ++ "," ++ String.fromFloat b ++ "]") model) ]
+            , row [ Font.size 15, width fill ] [ html <| chart model ]
             ]
         ]
     }
+
+
+chart : Model -> Svg msg
+chart model =
+    viewCustom
+        { x = Axis.full 1200 "Number" first
+        , y = Axis.full 600 "Close" second
+        , container = Container.default "line-chart-1"
+        , interpolation = Interpolation.default
+        , intersection = Intersection.default
+        , legends = Legends.default
+        , events = Events.default
+        , junk = Junk.default
+        , grid = Grid.dots 1 LCColors.gray
+        , area = Area.default
+        , line = Line.default
+        , dots = Dots.default
+        }
+        [ LineChart.line LCColors.purple Dots.circle "Number" (first model)
+        , LineChart.line LCColors.blueLight Dots.diamond "Close" (second model)
+        ]
